26e72b255e958bd43a08deaa2b727e94110a638b
Rdughan <Steppingforachange@gmail.com>
2022-09-11 04:54:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+char k;
+
+for (k = 'a'; k <= 'z'; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
