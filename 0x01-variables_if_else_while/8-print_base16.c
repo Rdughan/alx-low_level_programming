0d41d51a4d03595fe5584c91ab009753dfe494da
Rdughan <Steppingforachange@gmail.com>
2022-09-11 05:19:08 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 48; i < 58; i++)
+	{
+
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}
