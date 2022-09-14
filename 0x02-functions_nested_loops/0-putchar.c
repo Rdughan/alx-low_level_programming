30074df82b5224ea806860ee937edb418fdf6244
Rdughan <Steppingforachange@gmail.com>
2022-09-14 01:49:55 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +17,5 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
