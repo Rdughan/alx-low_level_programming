e199348c8a46ef60ec259b89ba04f10aff59a2ae
Rdughan <Steppingforachange@gmail.com>
2022-09-15 22:06:35 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * pint_numbers - prints 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i - 0; i < 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_pitchar('\n');
+}
